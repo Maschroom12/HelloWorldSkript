941cb10f5d752dd9479798b835b0470d11527017
Maschroom12 <maschroom12@gmail.com>
2025-06-06 15:59:24 +0200

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float spped;
+    public Transform target;
+    public int playerDamage = 2;
+    
+    void Update(){
+        transform.position = Vector3.MoveTowards(transform.position, target.position, spped * Time.deltaTime);
+        transform.LookAt(target.position);
+    }
+
+    private void OnTriggerEnter(Collider other){
+        Player player = other.GetComponent<Player>();
+        player.TakeDamge(playerDamage);
+    }
+}
